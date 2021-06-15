1cf2db7c5494198139a09df7d95006efb7bdde1d
Kristen <kristenwood123@gmail.com>
2021-06-14 19:22:51 -0700

    added bcrypt

--- server.js
+++ server.js
@@ -1,29 +1,45 @@
 const express = require('express')
 const chalk = require('chalk')
 const app = express()
+const bcrypt = require('bcrypt')
+app.use(express.json())
 
-const posts = [
-  {
-    username: 'Kristen', 
-    title: 'Post 1'
-  },
-   {
-    username: 'Jim', 
-    title: 'Post 2'
+const users = []
+
+// const posts = [
+//   {
+//     username: 'Kristen', 
+//     title: 'Post 1'
+//   },
+//    {
+//     username: 'Jim', 
+//     title: 'Post 2'
+//   }
+// ]
+
+// app.get('/posts', (req, res) => {
+//   res.json(posts)
+// })
+
+
+
+app.post('/users', async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt()
+    const hashedPassword = await bcrypt.hash(req.body.password, salt)
+    console.log(salt, hashedPassword);
+
+    const user = {name: req.body.name, password: hashedPassword} 
+    
+    users.push(user)
+    res.status(201).send(users)
   }
-]
-
-
-app.get('/posts', (req, res) => {
-  res.json(posts)
+  catch(e) {
+    res.status(500).send()
+  }
 })
 
-app.post('/users', (req, res) => {
-  const user = {
-    name: req.body.name,
-    password: req.body.password
-  }
-})
+
 
 app.get('/login', (req, res) => {
   //Authenticate
