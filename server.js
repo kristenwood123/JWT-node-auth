119bfbd77d7d26ed87cf9999b4cb3c702f028aad
Kristen <kristenwood123@gmail.com>
2021-06-14 18:44:55 -0700

    added REST response

--- server.js
+++ server.js
@@ -12,10 +12,22 @@ const posts = [
     title: 'Post 2'
   }
 ]
+
+
 app.get('/posts', (req, res) => {
   res.json(posts)
 })
 
+app.post('/users', (req, res) => {
+  const user = {
+    name: req.body.name,
+    password: req.body.password
+  }
+})
+
+app.get('/login', (req, res) => {
+  //Authenticate
+})
 
 app.listen(3000, () => {
   console.log(chalk.black.bgCyan('app listening on port 3000'))
